c25ea3dd689e8ea5a48874921d373a8595922c1e
Bruno-Henrique-BR <brunohenrique2208sb@gmail.com>
2021-06-22 14:12:19 -0300

    first commit

---
+++ src/main/java/com/valdir/hp/resources/ChamadoResource.java
@@ -0,0 +1,79 @@
+package com.valdir.hp.resources;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.valdir.hp.dtos.ChamadoDTO;
+import com.valdir.hp.model.Chamado;
+import com.valdir.hp.services.ChamadoService;
+
+@RestController
+@RequestMapping(value = "/chamados")
+public class ChamadoResource {
+
+	@Autowired
+	private ChamadoService service;
+	
+	/**
+	 * Busca um Chamado por ID
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id) {
+		Chamado obj = service.findById(id);
+		return ResponseEntity.ok().body(new ChamadoDTO(obj));
+	}
+
+	/**
+	 * Lista todos os Chamados do banco
+	 * 
+	 * @return
+	 */
+	@GetMapping
+	public ResponseEntity<List<ChamadoDTO>> findAll() {
+		List<Chamado> list = service.findAll();
+		List<ChamadoDTO> listDTO = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	/**
+	 * Cria um novo chamado
+	 * @param objDTO
+	 * @return
+	 */
+	@PostMapping
+	public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO objDTO) {
+		Chamado newObj = service.create(objDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).body(new ChamadoDTO(newObj));
+	}
+	
+	/**
+	 * Atualiza Chamado
+	 * @param id
+	 * @param objDTO
+	 * @return
+	 */
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ChamadoDTO> update(@PathVariable Integer id, @Valid @RequestBody ChamadoDTO objDTO) {
+		Chamado obj = service.update(id, objDTO);
+		return ResponseEntity.ok().body(new ChamadoDTO(obj));
+	}
+	
+}
